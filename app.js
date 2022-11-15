68eafc6120f2b415dca515e63368ece26847b9b1
petarpetarpetar <petar_ug01@yahoo.com>
2022-11-15 19:24:29 +0100

    routes created

--- app.js
+++ app.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.json());
+
+app.post('/order', (req, res) => {
+
+    //req.body
+    res.status(200);
     res.send('Hello World!')
+})
+
+app.get('/orderbook', (req, res) => {
+
+    //req.body
+    res.status(200);
+    res.send('Hello World!')
+})
+
+app.get('/order/:ID', (req, res) => {
+    res.status(200);
+    res.send("Hello World!")
+})
+
+app.delete('/order/all', (req, res) => {
+
+    res.status(200);
+    res.send("Hello World!");
 })
 
 app.listen(port, () => {
